534064f908f15dd38456713a3f14792617604b2b
Guillermo Correa <guillermo.correa.99@gmail.com>
2020-05-17 17:23:36 -0500

    added comments functionality, SignalR back and front

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,6 @@
 using System.Text;
+using System.Threading.Tasks;
+using API.Controllers.SignalR;
 using API.Middlewares;
 using Application.Activities;
 using Application.Interfaces;
@@ -44,11 +46,14 @@ namespace API
             {
                 opt.AddPolicy("CorsPolicy", policy =>
                 {
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                    policy.AllowAnyHeader().AllowAnyMethod()
+                        .WithOrigins("http://localhost:3000")
+                        .AllowCredentials();
                 });
             });
             services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddAutoMapper(typeof(List.Handler).Assembly);
+            services.AddSignalR();
             services.AddControllers(options =>
             {
                 var policyBuilder = new AuthorizationPolicyBuilder().RequireAuthenticatedUser();
@@ -85,6 +90,20 @@ namespace API
                         ValidateAudience = false,
                         ValidateIssuer = false
                     };
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnMessageReceived = context =>
+                        {
+                            var accessToken = context.Request.Query["access_token"];
+                            var path = context.HttpContext.Request.Path;
+                            if (!string.IsNullOrEmpty(accessToken) && path
+                                .StartsWithSegments("/chat"))
+                            {
+                                context.Token = accessToken;
+                            }
+                            return Task.CompletedTask;
+                        }
+                    };
                 });
 
             services.AddScoped<IJwtGenerator, JwtGenerator>();
@@ -113,6 +132,7 @@ namespace API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("/chat");
             });
         }
     }
